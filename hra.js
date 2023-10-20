9f3d4795aa14c712c0a7484e2a5e1b9af2d1ae1b
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-10-20 09:22:44 +0200

    refactoring

--- hra.js
+++ hra.js
@@ -4,7 +4,6 @@ let currentPlayer = 'circle';
 const playerElm = document.querySelector('.game-card__player');
 const buttonsElm = document.querySelectorAll('button');
 
-//fce obsahující přidání x/o, vytvoření pole, alert
 const addCircleOrCross = async (e) => {
   e.target.disabled = true;
   //Přidání 'o'
@@ -13,13 +12,45 @@ const addCircleOrCross = async (e) => {
       (currentPlayer = 'cross'),
       e.target.classList.add('game-card__box--circle');
   } else {
-    //Přidání 'x'
+    //Přidání 'x' po zahrání AI
     (playerElm.src = `images/circle.svg`),
       (currentPlayer = 'circle'),
       e.target.classList.add('game-card__box--cross');
   }
-  //vytvoření nového pole po každém tahu
-  const gameField = Array.from(buttonsElm).map((button) => {
+  //Nové pole po tahu
+  const gameField = newField();
+
+  //Alert VYHLÁŠENÍ VÍTĚZE
+  const winner = findWinner(gameField);
+  if (winner === 'o') {
+    alertAndRefresh('Vyhrálo kolečko 🥳!');
+  } else if (winner === 'x') {
+    alertAndRefresh('Vyhrál křížek 🥳!');
+  } else if (winner === 'tie') {
+    alertAndRefresh('Hra skončila nerozhodně😒!');
+  } else if (currentPlayer === 'cross') {
+    //Tah AI
+    aiBotTurn(gameField);
+  }
+};
+
+//-------------------POMOCNÉ FCE---------------------------
+//POSLUCHAČ NA TLAČÍTKA
+buttonsElm.forEach((button) => {
+  button.addEventListener('click', addCircleOrCross);
+});
+
+//ALERT PRO PŘERUŠENÍ HRY
+document.getElementById('button-blue').addEventListener('click', (event) => {
+  if (confirm('Opravdu chceš začít znovu?')) {
+    return;
+  }
+  return event.preventDefault();
+});
+
+//VYKRESLENÍ NOVÉHO POLE
+const newField = () => {
+  return Array.from(buttonsElm).map((button) => {
     if (button.classList.contains('game-card__box--circle')) {
       return 'o';
     }
@@ -28,49 +59,30 @@ const addCircleOrCross = async (e) => {
     }
     return '_';
   });
-
-  //Alert na vyhlášení vítěze
-  const winner = findWinner(gameField);
-  const alertAndRefresh = (message) => {
-    setTimeout(() => {
-      alert(message);
-      window.location.reload();
-    }, 500);
-  };
-  if (winner === 'o') {
-    alertAndRefresh('Vyhrálo kolečko 🥳!');
-  } else if (winner === 'x') {
-    alertAndRefresh('Vyhrál křížek 🥳!');
-  } else if (winner === 'tie') {
-    alertAndRefresh('Hra skončila nerozhodně😒!');
-  } else if (currentPlayer === 'cross') {
-    //napojení API hrající za X
-    const response = await fetch(
-      'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
-      {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify({ board: gameField, player: 'x' }),
-      },
-    );
-    const data = await response.json();
-    const { x, y } = data.position;
-    const field = buttonsElm[x + y * 10];
-    field.click();
-  }
 };
 
-//Posluchač na tlačítka
-buttonsElm.forEach((button) => {
-  button.addEventListener('click', addCircleOrCross);
-});
+//NAPOJENÍ AI HRAJÍCÍ ZA X
+const aiBotTurn = async (array) => {
+  const response = await fetch(
+    'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ board: array, player: 'x' }),
+    },
+  );
+  const data = await response.json();
+  const { x, y } = data.position;
+  const field = buttonsElm[x + y * 10];
+  field.click();
+};
 
-//Alert při přerušení hry
-document.getElementById('button-blue').addEventListener('click', (event) => {
-  if (confirm('Opravdu chceš začít znovu?')) {
-    return;
-  }
-  return event.preventDefault();
-});
+// ČASOVAČ SE ZPRÁVOU, KDO VYHRÁL
+const alertAndRefresh = (message) => {
+  setTimeout(() => {
+    alert(message);
+    window.location.reload();
+  }, 500);
+};
